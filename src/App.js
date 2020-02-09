868b4b078e118df890f042eb3387912357e55040
inder2108 <inderjit.bhogal89@gmail.com>
2020-02-09 22:49:47 +0530

    Removed unnecessary code and started with TODO items for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+          <li>Prettier Code Formating and airbnb styling</li>
+          <li>CMS using firebase: https://hackernoon.com/how-i-built-a-content-management-system-for-a-react-app-in-one-day-269df17f5509</li>
+          <li>Bootstrap 4 integration</li>
+          <li>Fontawesome 5 integration</li>
+          <li>React routing integration</li>
+          <li>Redux integration - Keep only global state in Redux and not the whole JSON response</li>
+        </ul>
     </div>
   );
 }
