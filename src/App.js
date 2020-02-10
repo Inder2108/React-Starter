87dfe944bb0653f83c5975552c27659b94e67ca3
inder2108 <inderjit.bhogal89@gmail.com>
2020-02-10 21:33:57 +0530

    Added node dependencies

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className="App">
       <ul>
-          <li>Prettier Code Formating and airbnb styling</li>
           <li>CMS using firebase: https://hackernoon.com/how-i-built-a-content-management-system-for-a-react-app-in-one-day-269df17f5509</li>
-          <li>Bootstrap 4 integration</li>
-          <li>Fontawesome 5 integration</li>
-          <li>React routing integration</li>
+          <li>Async Await Integration</li>
+          <li>React routing integration - Static/dynamic routes as described in deployment section of create react app</li>
           <li>Redux integration - Keep only global state in Redux and not the whole JSON response</li>
         </ul>
     </div>
