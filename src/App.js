f8fd34e31666725d4ba69b77e25e002c77355cd8
inder2108 <inderjit.bhogal89@gmail.com>
2020-02-25 22:14:37 +0530

    Redux introduction and underscore in package.json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers/Reducers.js";
+import { Provider } from "react-redux";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
 import './App.css';
+
+const loggerMiddleware = createLogger();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" store={store}>
       <ul>
         <li>CMS using firebase: https://hackernoon.com/how-i-built-a-content-management-system-for-a-react-app-in-one-day-269df17f5509</li>
         <li>Async Await Integration</li>
-        <li>Redux integration - Keep only global state in Redux and not the whole JSON response</li>
+        <li>Router either based on hash or direct / based</li>
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
 import './index.scss';
 import App from './App';
 
