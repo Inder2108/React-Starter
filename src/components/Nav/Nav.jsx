8d0061b31bd21dc9582299b282a372c80fa8464e
inder2108 <inderjit.bhogal89@gmail.com>
2020-03-11 23:44:04 +0530

    Updated Nav.jsx to include fixed top styling and dynamic routing

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,59 +1,34 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
+import history from '../../history';
+
+let navLinkJSX = (link, text) => (
+    <li class="nav-item">
+        <a class="nav-link" onClick={e => { e.preventDefault(); history.push(link); }}>{text}</a>
+    </li>
+);
 
 const Nav = ({ isAuthenticated, uuid }) => {
-    const logInOut = isAuthenticated ? (
-        <li>
-            <NavLink to="/log-out">
-                Log out
-            </NavLink>
-        </li>
-    ) : (
-            <li>
-                <NavLink to="/log-in">
-                    Log in
-                </NavLink>
-            </li>
-        );
-    const mainLinks = isAuthenticated ? (
-        <li>
-            <NavLink to="/home">
-                Home
-            </NavLink>
-        </li>
-    ) : (
-            <>
-                <li>
-                    <NavLink to="/">
-                        Landing
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">
-                        About
-                    </NavLink>
-                </li>
-            </>
-        );
-    return (
+    const logInOut = isAuthenticated ? navLinkJSX("/log-out", "Log Out") : navLinkJSX("/log-in", "Log In");
+    const mainLinks = isAuthenticated ? navLinkJSX("/home", "Home") : (
         <>
-            <p>Auth state: {isAuthenticated ? `Logged in user: ${uuid}` : "Logged out"}</p>
-            <ul>
+            {navLinkJSX("/", "Landing")}
+            {navLinkJSX("/about", "About")}
+        </>
+    );
+    return (
+        <div class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <p></p>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link">Auth state: {isAuthenticated ? `Logged in user: ${uuid}` : "Logged out"}</a>
+                </li>
                 {mainLinks}
-                <li>
-                    <NavLink to="/terms">
-                        Terms
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/broken-link">
-                        Broken link
-                    </NavLink>
-                </li>
+                {navLinkJSX("/terms", "Terms")}
+                {navLinkJSX("/broken-link", "Broken Link")}
                 {logInOut}
             </ul>
-        </>
+        </div>
     );
 };
 const mapStateToProps = (state) => ({
