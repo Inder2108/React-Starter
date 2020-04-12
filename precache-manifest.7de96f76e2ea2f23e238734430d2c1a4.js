9fb07e7f2933cceaa81deb8917c8cf434c12545f
inder2108 <inderjit.bhogal89@gmail.com>
2020-04-12 21:20:43 +0530

    Updates

---
+++ precache-manifest.7de96f76e2ea2f23e238734430d2c1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b6e4efa8f6c7a2e8bc6070859752e1",
+    "url": "/React-Starter/index.html"
+  },
+  {
+    "revision": "d78bf3a158d38fa2734f",
+    "url": "/React-Starter/static/css/main.9ba5e87f.chunk.css"
+  },
+  {
+    "revision": "07bef83cc888427d2d20",
+    "url": "/React-Starter/static/js/2.cfc80bdc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-Starter/static/js/2.cfc80bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78bf3a158d38fa2734f",
+    "url": "/React-Starter/static/js/main.4f7e6de2.chunk.js"
+  },
+  {
+    "revision": "afc850925e2282536f7e",
+    "url": "/React-Starter/static/js/runtime-main.eb7978d2.js"
+  }
+]);
